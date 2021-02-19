ec6f3a00de66389c8eab01587452b8d8e283daac
Ingrid <ingrid.danielle@gmail.com>
2021-02-18 22:54:15 -0300

    Implemented profile screen

---
+++ app/src/main/java/com/ingrid/mundoanimal/activities/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.ingrid.mundoanimal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ingrid.mundoanimal.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
